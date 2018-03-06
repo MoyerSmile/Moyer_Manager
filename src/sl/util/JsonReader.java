940db8de573524f29e9c5c89b2734ea914654de7
moyer <mojp@sanly.com.cn>
2018-03-06 22:02:19 +0800

    格式化了一下

--- src/sl/util/JsonReader.java
+++ src/sl/util/JsonReader.java
@@ -10,31 +10,27 @@ import javax.servlet.http.HttpServletRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
-
-
-
-
 public class JsonReader {
-	 public static JSONObject receivePost(HttpServletRequest request) throws IOException, UnsupportedEncodingException {  
-		  
-	        // 读取请求内容  
-	        BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream(),"utf-8"));  
-	        String line = null;  
-	        StringBuilder sb = new StringBuilder();  
-	        while ((line = br.readLine()) != null) {  
-	            sb.append(line);  
-	        }  
-	        //将json字符串转换为json对象  
-	        JSONObject json;
-			try {
-				json = new JSONObject(sb.toString());
-				return json;  
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return null;
-			}
-	        
-	    }  
+    public static JSONObject receivePost(HttpServletRequest request)
+            throws IOException, UnsupportedEncodingException {
+        // 读取请求内容
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(request.getInputStream(), "utf-8"));
+        String line = null;
+        StringBuilder sb = new StringBuilder();
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        // 将json字符串转换为json对象
+        JSONObject json;
+        try {
+            json = new JSONObject(sb.toString());
+            return json;
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+
+    }
 }
